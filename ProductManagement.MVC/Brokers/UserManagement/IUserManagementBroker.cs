a96daaa167bf124e5b32be9c6a889bae8fab733d
Sokhib Jaynarov <s.jaynarov@baik.uz>
2023-08-06 13:38:57 +0500

    BROKER: UserManagement Broker Added

---
+++ ProductManagement.MVC/Brokers/UserManagement/IUserManagementBroker.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using System;
+using ProductManagement.MVC.Models;
+
+namespace ProductManagement.MVC.Brokers.UserManagement
+{
+    public interface IUserManagementBroker
+    {
+        ValueTask<ApplicationUser> InsertUserAsync(ApplicationUser user, string password);
+        IQueryable<ApplicationUser> SelectAllUsers();
+        ValueTask<ApplicationUser> SelectUserByIdAsync(Guid userId);
+        ValueTask<ApplicationUser> UpdateUserAsync(ApplicationUser user);
+        ValueTask<ApplicationUser> DeleteUserAsync(ApplicationUser user);
+    }
+}
---
+++ ProductManagement.MVC/Brokers/UserManagement/UserManagementBroker.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Linq;
+using System.Threading.Tasks;
+using System;
+using ProductManagement.MVC.Models;
+
+namespace ProductManagement.MVC.Brokers.UserManagement
+{
+    public class UserManagementBroker : IUserManagementBroker
+    {
+        private readonly UserManager<ApplicationUser> userManagement;
+
+        public UserManagementBroker(UserManager<ApplicationUser> userManager)
+        {
+            this.userManagement = userManager;
+        }
+        public IQueryable<ApplicationUser> SelectAllUsers() => this.userManagement.Users;
+
+        public async ValueTask<ApplicationUser> SelectUserByIdAsync(Guid userId)
+        {
+            var broker = new UserManagementBroker(this.userManagement);
+
+            return await broker.userManagement.FindByIdAsync(userId.ToString());
+        }
+
+        public async ValueTask<ApplicationUser> InsertUserAsync(ApplicationUser user, string password)
+        {
+            var broker = new UserManagementBroker(this.userManagement);
+            await broker.userManagement.CreateAsync(user, password);
+
+            return user;
+        }
+
+        public async ValueTask<ApplicationUser> UpdateUserAsync(ApplicationUser user)
+        {
+            var broker = new UserManagementBroker(this.userManagement);
+            await broker.userManagement.UpdateAsync(user);
+
+            return user;
+        }
+
+        public async ValueTask<ApplicationUser> DeleteUserAsync(ApplicationUser user)
+        {
+            var broker = new UserManagementBroker(this.userManagement);
+            await broker.userManagement.DeleteAsync(user);
+
+            return user;
+        }
+    }
+}
