3c4139f58e8b2d88d9bac89e8d22e82e2f825e8d
Sokhib Jaynarov <s.jaynarov@baik.uz>
2023-08-07 21:28:30 +0500

    CONTROLLER: Order CRUD Added

--- ProductManagement.MVC/Controllers/OrderController.cs
+++ ProductManagement.MVC/Controllers/OrderController.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ProductManagement.MVC.Brokers.UserManagement;
 using ProductManagement.MVC.Services.Companies;
 using ProductManagement.MVC.Services.Orders;
+using System.Threading.Tasks;
+using System;
+using ProductManagement.MVC.Models;
 
 namespace ProductManagement.MVC.Controllers
 {
@@ -17,10 +21,52 @@ namespace ProductManagement.MVC.Controllers
             this.companyService = companyService;
         }
 
+        // GET: Orders 
         public IActionResult Index()
         {
             var orders = orderService.RetrieveAllOrders();
             return View(orders);
+        }
+
+        // GET: For details order
+        public async Task<ActionResult> Details(Guid Id)
+        {
+            var order = await orderService.RetrieveOrderByIdAsync(Id);
+
+            return View(order);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(Order order)
+        {
+            await orderService.AddOrderAsync(order);
+            ViewBag.Message = "Data Insert Successfully";
+            return RedirectToAction("Index");
+        }
+
+        // GET: For edit user
+        public async Task<ActionResult> Edit(Guid Id)
+        {
+            var order = await orderService.RetrieveOrderByIdAsync(Id);
+
+            return View(order);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Edit(Order order)
+        {
+            var existOrder = await orderService.RetrieveOrderByIdAsync(order.OrderId);
+
+
+            await orderService.ModifyOrderAsync(existOrder);
+
+            return RedirectToAction("Index");
         }
     }
 }
